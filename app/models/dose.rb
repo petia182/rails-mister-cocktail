0cf232cda789f6095f9563a8ba5b5c4ce9c16b08
Petya Nikolova <petia_182@yahoo.com>
2016-11-10 19:03:46 +0000

    Validates dose description

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,4 +3,6 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
 
   validates :cocktail_id, uniqueness: { scope: :ingredient_id }
+  validates :description, presence: true
+  validates :ingredient, presence: true
 end
